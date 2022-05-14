222ddc8b0d92f20f140ca2d1d5e27f5358afe9a9
Lexermal <48994900+lexermal@users.noreply.github.com>
2022-05-14 09:54:26 +0200

    added restriction to not use underscores in hostnames

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,17 @@
 
 # check if host name variable is set and change the name if it is set
 if [[ ! -z "${SYSBOX_HOST}" ]] && [[ "$SYSBOX_HOST" != "$HOSTNAME" ]]; then
+
+  if [[ "$SYSBOX_HOST" == *"_"* ]]; then
+    echo "The hostname is not allowed to contain underscores. The set hostname is '$SYSBOX_HOST'."
+    exit 1
+  fi
+
   echo "Setting hostname to '$SYSBOX_HOST'."
   sudo echo "$SYSBOX_HOST" > /etc/hostname
   echo $(hostname -I | cut -d\  -f1) $SYSBOX_HOST | sudo tee -a /etc/hosts
   echo 127.0.0.1 $SYSBOX_HOST | sudo tee -a /etc/hosts
+  echo 127.0.1.1 $SYSBOX_HOST.example.com $SYSBOX_HOST >> /etc/hosts
 fi
 
 # check if admin password variable is set and change the name if it is set
